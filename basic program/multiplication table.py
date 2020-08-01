77e1beed858723637c1c24b47b2935d14447e77d
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2020-08-01 13:14:54 +0530

    Add files via upload
    create a basic multiplication table, aimed at boosting confidence to code.

---
+++ basic program/multiplication table.py
@@ -0,0 +1,6 @@
+while True:
+    num1=int(input("Enter the number whose multiplication table is to found: "))
+    for i in range(1,21):
+        print(" {0} X {1} = {2} ".format(num1,i,(num1*i)))
+    print("----------------------------")
+    continue
