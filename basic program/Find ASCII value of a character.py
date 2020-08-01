47e35ffa10d38e14757a5ebf92f59613e76a5bc0
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2020-08-01 13:07:20 +0530

    Add files via upload
    understanding how ASCII value is used to represent characters and many more.

---
+++ basic program/Find ASCII value of a character.py
@@ -0,0 +1,4 @@
+"""ASCII: ASCII is an acronym stands for American Standard Code for Information Interchange. In ASCII, a specific numerical value is given to different characters and symbols, for computers to store and manipulate.
+It is case sensitive. Same character, having different format (upper case and lower case) has different value. For example: The ASCII value of "A" is 65 while the ASCII value of "a" is 97."""
+c=input("Enter a character: ")
+print(f"The ASCII value of {c} is {ord(c)}")
